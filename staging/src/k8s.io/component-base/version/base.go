c20653d8c9de5744b75a2efe5f91c21326a8f890
Brad Davidson <brad.davidson@rancher.com>
2020-11-23 16:40:58 -0800

    v1.20.0-beta.2-k3s1

--- staging/src/k8s.io/component-base/version/base.go
+++ staging/src/k8s.io/component-base/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "20"
-	gitVersion   = "v1.20.0-beta.1-k3s1"
-	gitCommit    = "dfe0ad6d261e638a1218430c8cb189e664d36a0a"
+	gitVersion   = "v1.20.0-beta.2-k3s1"
+	gitCommit    = "2f86a814e32b96e25af90a41ac783d43f739bffd"
 	gitTreeState = "clean"
-	buildDate = "2020-11-04T23:38:10Z"
+	buildDate = "2020-11-24T00:37:49Z"
 )
