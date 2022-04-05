9d800277e166d9f546972b6e17b7386f0d7ac915
Brad Davidson <brad.davidson@rancher.com>
2022-04-05 17:57:16 +0000

    v1.24.0-beta.0-k3s1

--- staging/src/k8s.io/client-go/pkg/version/base.go
+++ staging/src/k8s.io/client-go/pkg/version/base.go
@@ -3,8 +3,8 @@ package version
 var (
 	gitMajor = "1"
 	gitMinor = "24"
-	gitVersion   = "v1.24.0-alpha.4-k3s1"
-	gitCommit    = "cb16473328b5c3a28508666e578bfeb93ad6f172"
+	gitVersion   = "v1.24.0-beta.0-k3s1"
+	gitCommit    = "7968cde595e06fd5bbc0aab03eff05d15bf46a62"
 	gitTreeState = "clean"
-	buildDate = "2022-03-25T23:58:10Z"
+	buildDate = "2022-04-05T17:54:26Z"
 )
